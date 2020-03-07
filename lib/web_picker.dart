cd24052c24619c35f912165b6cb3d1041a263002
Gabriele Tramonte <gabry.t92@gmail.com>
2020-03-07 19:33:39 +0100

    switch to image_picker_web for compatibility reason

--- lib/web_picker.dart
+++ lib/web_picker.dart
@@ -2,23 +2,14 @@ import 'dart:html';
 
 import 'package:cross_picker/cross_picker.dart';
 import 'package:flutter/src/widgets/image.dart';
-import 'package:universal_html/prefer_universal/html.dart' as html;
+import 'package:image_picker_web/image_picker_web.dart';
+//import 'package:universal_html/prefer_universal/html.dart' as html;
 
 class WebPicker implements CrossPicker {
   @override
   Future<Image> getImage() async {
-    html.InputElement uploadInput = html.FileUploadInputElement();
-    uploadInput.multiple = true;
-    uploadInput.draggable = true;
-    uploadInput.click();
-    uploadInput.onChange.listen((e) {
-      final files = uploadInput.files;
-      final file = files[0];
-      final reader = new html.FileReader();
-      reader.onLoadEnd.listen((e) {
-
-      });
-    });
+   Image image = await ImagePickerWeb.getImage();
+   return image;
   }
 }
 CrossPicker getPicker() => WebPicker();
