c28f96c8fa2b848f0210052a1f7a95979f914cdf
Gabriele Tramonte <gabry.t92@gmail.com>
2020-03-08 11:34:15 +0100

    Revert "switch to image_picker_web for compatibility reason"
    This reverts commit cd24052c

--- lib/web_picker.dart
+++ lib/web_picker.dart
@@ -1,13 +1,30 @@
 import 'dart:convert';
 import 'dart:io';
+
 import 'package:cross_picker/cross_picker.dart';
 import 'package:universal_html/prefer_universal/html.dart' as html;
 
 class WebPicker implements CrossPicker {
   @override
-  Future<Image> getImage() async {
-   Image image = await ImagePickerWeb.getImage();
-   return image;
+  Future<File> getImage() async {
+    File selectedFile;
+    html.InputElement uploadInput = html.FileUploadInputElement();
+    uploadInput.multiple = true;
+    uploadInput.draggable = true;
+    uploadInput.click();
+    uploadInput.onChange.listen((e) {
+      final files = uploadInput.files;
+      final file = files[0];
+      final reader = new html.FileReader();
+      reader.onLoadEnd.listen((e) {
+        final result = reader.result;
+        final bytesData =
+            Base64Decoder().convert(result.toString().split(",").last);
+        selectedFile = File("uploaded-${DateTime.now()}");
+        selectedFile.writeAsBytesSync(bytesData);
+      });
+    });
+    return selectedFile;
   }
 }
 CrossPicker getPicker() => WebPicker();
